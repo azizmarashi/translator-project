9c3dc5f1dea94de37bd47a802e611ebc64c053f4
Sam Hosseini <sam.hosseini256@gmail.com>
2023-11-13 08:30:38 +0330

    complete service

--- src/main/java/com/example/translatorproject/TranslatorProjectApplication.java
+++ src/main/java/com/example/translatorproject/TranslatorProjectApplication.java
@@ -3,9 +3,8 @@ package com.example.translatorproject;
 public class TranslatorProjectApplication {
 
     public static void main(String[] args) {
-        System.out.println(TranslatorService.translateText("اسم من علی است"));
 
-        System.out.println(TranslatorService.getImageLinksByKeyword("book"));
+        System.out.println(TranslatorService.getImageLinkPerWord("اسم من علی است"));
 
     }
 
--- src/main/java/com/example/translatorproject/TranslatorService.java
+++ src/main/java/com/example/translatorproject/TranslatorService.java
@@ -3,11 +3,9 @@ package com.example.translatorproject;
 import com.darkprograms.speech.translator.GoogleTranslate;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
-import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
 
 public class TranslatorService {
 
@@ -31,11 +29,25 @@ public class TranslatorService {
             try {
                 doc = Jsoup.connect(url).get();
                 successConnect = true;
-            } catch (IOException e) {
-            }
+            } catch (IOException e) {}
         }
         Elements figures = doc.select("figure");
         return figures.get(0).selectFirst("link").attr("href");
+    }
+
+    public static List<String> separateWords(String sentence){
+        sentence = sentence.replace("."," ");
+        return List.of(sentence.split(" "));
+    }
+
+    public static Map getImageLinkPerWord(String firstSentence){
+        String translatedSentence = translateText(firstSentence);
+        List<String> separateWords = separateWords(translatedSentence);
+        Map<String,String> wordAndLink = new TreeMap<>();
+        for (String word: separateWords){
+            wordAndLink.put(word,getImageLinksByKeyword(word));
+        }
+        return wordAndLink;
     }
 
 }
