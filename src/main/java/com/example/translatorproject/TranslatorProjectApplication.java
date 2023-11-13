8b020fa8f5d96ee43b701849881cc7ae2864c263
Sam Hosseini <sam.hosseini256@gmail.com>
2023-11-13 07:56:16 +0330

    add get image link by keyword to service

--- src/main/java/com/example/translatorproject/TranslatorProjectApplication.java
+++ src/main/java/com/example/translatorproject/TranslatorProjectApplication.java
@@ -3,6 +3,9 @@ package com.example.translatorproject;
 public class TranslatorProjectApplication {
 
     public static void main(String[] args) {
+        System.out.println(TranslatorService.translateText("اسم من علی است"));
+
+        System.out.println(TranslatorService.getImageLinksByKeyword("book"));
 
     }
 
--- src/main/java/com/example/translatorproject/TranslatorService.java
+++ src/main/java/com/example/translatorproject/TranslatorService.java
@@ -1,7 +1,13 @@
 package com.example.translatorproject;
 
 import com.darkprograms.speech.translator.GoogleTranslate;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 public class TranslatorService {
 
@@ -14,6 +20,22 @@ public class TranslatorService {
             e.printStackTrace();
         }
         return response;
+    }
+
+    public static String getImageLinksByKeyword(String keyword) {
+        String url = "https://www.peakpx.com/en/search?q=" + keyword;
+        Document doc = null;
+        boolean successConnect = false;
+        List<String> imageLinks = new ArrayList<>();
+        while (!successConnect) {
+            try {
+                doc = Jsoup.connect(url).get();
+                successConnect = true;
+            } catch (IOException e) {
+            }
+        }
+        Elements figures = doc.select("figure");
+        return figures.get(0).selectFirst("link").attr("href");
     }
 
 }
